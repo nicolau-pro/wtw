201d3b6dc907a3e26b20508e3db3a469e1a1c467
nicolau.pro <radu.alex.nicolau@gmail.com>
2019-10-13 16:28:46 +0100

    Tablet classes
    responsive clients menu

--- src/components/Home/List/Item.jsx
+++ src/components/Home/List/Item.jsx
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 class ListItem extends Component {
   render() {
     const client = this.props.client;
-    const className = "clientListItem" + (client.selected ? " selected" : "");
+    const className =
+      "clientListItem" + (client.selected ? " selected" : " d-none d-lg-block");
     return (
       <li className={className}>
         <img src={client.logo} alt={client.name} />
